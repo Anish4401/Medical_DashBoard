8d19e3d06b9414b1d197367e86bc538fd34cca7b
Anish kumar yadav <107028533+Anish4401@users.noreply.github.com>
2024-12-22 00:34:39 +0530

    Update medicieneRoute.js

--- src/routes/medicieneRoute.js
+++ src/routes/medicieneRoute.js
@@ -3,30 +3,8 @@ const router = express.Router();
 const Medicine = require("../models/Medicine");
 const medicieneController = require("../controllers/medicieneController");
 
-// Add New Medicine
-router.post("/", async (req, res) => {
-  const { name, cost, expiry_date, discount, gst_applicable } = req.body;
 
-  if (!name || !cost || !expiry_date) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-
-  try {
-    const newMedicine = new Medicine({
-      name,
-      cost,
-      expiry_date,
-      discount,
-      gst_applicable,
-    });
-    const savedMedicine = await newMedicine.save();
-    res.status(201).json(savedMedicine);
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-//Get all the Listed Medicine
+router.post("/", medicieneController.addMedicine) 
 router.get("/", async (req, res) => {
   try {
     const medicines = await Medicine.find();
