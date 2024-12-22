1d619835200f8d562625ce6007bec7400fa34102
Anish kumar yadav <107028533+Anish4401@users.noreply.github.com>
2024-12-22 17:23:39 +0530

    Update saleRoute.js

--- src/routes/saleRoute.js
+++ src/routes/saleRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const saleController = require("../controllers/SaleController");
 
 router.post("/sales", saleController.recordSale);
-router.get('/',sales.controller.getMedicine);
+router.get('/', saleController.getMedicine);
 router.get("/sales/daily", saleController.getDailySales);
 router.get("/sales/monthly", saleController.getMonthlySales);
 
